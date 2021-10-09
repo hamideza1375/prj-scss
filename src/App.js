2cde45bbafb524e887d003c19f823fc8fa87be2c
hamid reza attar <reza.attar1375@gmail.com>
2021-10-09 16:50:27 +0330

    sass all

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './sass/App.css';
 
 function App() {
   return (
-    <div>
-      <div >
-dd
-      </div>
+    <div className='D-flex-row'>
+
+      <div className='Col-4'>flex-scss </div>
+      <div className='Col-4'>flex-scss </div> 
+      <div className='Col-4'>flex-scss </div>
+      <div className='Col-4'>flex-scss </div>  
+      <div className='Col-4'>flex-scss </div>
+      <div className='Col-4'>flex-scss </div>  
+      <div className='Col-4'>flex-scss </div>
+      <div className='Col-4'>flex-scss </div>
+
     </div>
   );
 }
