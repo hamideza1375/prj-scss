195b375d01bdbf88b78416b4bee0bd20474dccde
hamid reza attar <reza.attar1375@gmail.com>
2021-10-09 21:15:58 +0330

    new scss2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import './sass/App.css';
+import './css/App.css';
 
 function App() {
   return (
     <div className='D-flex-row'>
 
-      <div className='Col-4'>flex-scss </div>
-      <div className='Col-4'>flex-scss </div> 
-      <div className='Col-4'>flex-scss </div>
-      <div className='Col-4'>flex-scss </div>  
-      <div className='Col-4'>flex-scss </div>
-      <div className='Col-4'>flex-scss </div>  
-      <div className='Col-4'>flex-scss </div>
-      <div className='Col-4'>flex-scss </div>
+    <div className='D-flex-row Col-all-6 bg-secondary'>
+      <div className='Col1 h-25 Col-all-6'>flex-scss111111 </div>
+      <div className='Col2 h-25 Col-all-6'>flex-scss222222 </div> 
+    </div>
 
+    <div className=' D-flex-row Col-all-6 bg-secondary'>
+      <div className='Col1 h-25 Col-all-6'>flex-scss33333 </div> 
+      <div className=' Col2 h-25 Col-all-6'>flex-scss444444 </div> 
+    </div>
+
+      {/* <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div> 
+      <div className=' Col-3'>flex-scss222222 </div>  */}
     </div>
   );
 }
